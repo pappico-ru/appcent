9330214718cc20d438b85349564c276f765eca15
Aleksandr Nikitin <a.nikitin@i.ua>
2015-05-18 06:15:49 +0300

    Update files

--- js/common.js
+++ js/common.js
@@ -11,7 +11,17 @@
 		acOpen: 'ac-open',
 		acHide: 'ac-hide',
 		acSentAppBtn: 'ac-sent-app-btn',
-		acInputBlock: 'ac-input-block'
+		acInputBlock: 'ac-input-block',
+
+		acMobileMenu: 'ac-mobile-menu',
+		acShowMenu: 'ac-show-menu',
+		acCloseMenu: 'ac-close-menu',
+
+		acIphone: 'ac-iphone',
+
+		acAppleBtn: 'ac-apple-btn',
+		acGoogleBtn: 'ac-google-btn',
+		acNoUrl: 'ac-no-url'
 	};
 
 	var ids = {};
@@ -37,12 +47,25 @@
 
 	var $acLangButton,
 		$acSentAppBtn,
-		$acInputBlock;
+		$acInputBlock,
+		$acAppleBtn,
+		$acGoogleBtn,
+		$acMobileMenu,
+		$acCloseMenu,
+		$acIphone,
+		$body,
+		$html;
 
 	$(function(){
 		$acLangButton = $(selectors.acLang).find('div');
 		$acSentAppBtn = $(selectors.acSentAppBtn);
 		$acInputBlock = $(selectors.acInputBlock);
+		$acAppleBtn = $(selectors.acAppleBtn);
+		$acMobileMenu = $(selectors.acMobileMenu);
+		$acCloseMenu = $(selectors.acCloseMenu);
+		$acIphone = $(selectors.acIphone);
+		$body = $('body');
+		$html = $('html');
 
 		$acLangButton.on('click', function(){
 			var $this = $(this);
@@ -53,5 +76,50 @@
 			$(this).addClass(classNames.acHide);
 			$acInputBlock.removeClass(classNames.acHide);
 		});
+
+		if ($html.hasClass('android')) {
+			$acAppleBtn.addClass(classNames.acNoUrl);
+			showWindowSend();
+		}
+
+		if ($html.hasClass('ios')) {
+			$acGoogleBtn.addClass(classNames.acNoUrl);
+			showWindowSend();
+		}
+
+		$acMobileMenu.on('click', function(){
+			$body.toggleClass(classNames.acShowMenu);
+		});
+
+		$acCloseMenu.on('click', function(){
+			$body.toggleClass(classNames.acShowMenu);
+		});
+		setNameLanguages();
 	});
+
+	$(window).on('resize', setNameLanguages);
+
+	var setNameLanguages = function() {
+		var $dataSelectLang = $('[data-select-lang="true"]'),
+			$documentWidth = $(document).width(),
+			readParameter;
+		if ($documentWidth <= 960) {
+			readParameter = 'full-text';
+		} else {
+			readParameter = 'short-text';
+		}
+		$dataSelectLang.each(function(){
+			var $this = $(this);
+			$this.text($this.data(readParameter));
+		})
+	};
+
+	var showWindowSend = function() {
+		$(selectors.acNoUrl).on('click', function(e){
+			var elem = e.target || e.srcElement;
+			e.preventDefault();
+			alert('No URL');
+			return false;
+		});
+	};
 })();
