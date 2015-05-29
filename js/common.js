c15884210b4844fa03b74aba445b97320fee9943
Aleksandr Nikitin <a.nikitin@i.ua>
2015-05-29 11:29:50 +0300

    End work

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
 	$.fn.exists = function () {
 		return this.length !== 0;
 	};
@@ -6,12 +6,15 @@
 	window.ac = {};
 
 	var classNames = {
-		displayNone: '_display_none',
+		hide: 'hide',
 		acLang: 'ac-lang',
 		acOpen: 'ac-open',
-		acHide: 'ac-hide',
 		acSentAppBtn: 'ac-sent-app-btn',
+		acPhoneInputBtn: 'ac-phone-input-btn',
 		acInputBlock: 'ac-input-block',
+		acSendMessageContainer: 'ac-send-message-container',
+
+		acPhoneInputBlock: 'ac-phone-input-block',
 
 		acMobileMenu: 'ac-mobile-menu',
 		acShowMenu: 'ac-show-menu',
@@ -20,8 +23,17 @@
 		acIphone: 'ac-iphone',
 
 		acAppleBtn: 'ac-apple-btn',
-		acGoogleBtn: 'ac-google-btn',
-		acNoUrl: 'ac-no-url'
+		acAndroidBtn: 'ac-android-btn',
+		acNoUrl: 'ac-no-url',
+
+		acSecond: 'ac-second',
+		acSendAgain: 'ac-send-again',
+		acClock: 'ac-clock',
+
+		acNoScroll: 'ac-no-scroll',
+
+		acVideoContainer: 'ac-video-container',
+		acVideo: 'ac-video'
 	};
 
 	var ids = {};
@@ -45,36 +57,38 @@
 
 	var selectors = ac.buildSelectors(classNames, ids);
 
-	var $acLangButton,
-		$acSentAppBtn,
-		$acInputBlock,
-		$acAppleBtn,
-		$acGoogleBtn,
-		$acMobileMenu,
-		$acCloseMenu,
-		$acIphone,
-		$body,
-		$html;
+	var $acLang = $(selectors.acLang),
+		$acSentAppBtn = $(selectors.acSentAppBtn),
+		$acPhoneInputBtn = $(selectors.acPhoneInputBtn),
+		$acSendMessageContainer = $(selectors.acSendMessageContainer),
+		$acInputBlock = $(selectors.acInputBlock),
+		$acAppleBtn = $(selectors.acAppleBtn),
+		$acAndroidBtn = $(selectors.acAndroidBtn),
+		$acPhoneInputBlock = $(selectors.acPhoneInputBlock),
+		$acMobileMenu = $(selectors.acMobileMenu),
+		$acCloseMenu = $(selectors.acCloseMenu),
+		$acIphone = $(selectors.acIphone),
+		$acSecond = $(selectors.acSecond),
+		$acSendAgain = $(selectors.acSendAgain),
+		$acClock = $(selectors.acClock),
+		$acVideo = $(selectors.acVideo),
+		$body = $('body'),
+		$html = $('html'),
+		$acPopupBox = $('.ac-popup-box'),
+		$acPopupClose = $('.ac-popup-close'),
+		defaultTime = 16,
+		second;
 
-	$(function(){
-		$acLangButton = $(selectors.acLang).find('div');
-		$acSentAppBtn = $(selectors.acSentAppBtn);
-		$acInputBlock = $(selectors.acInputBlock);
-		$acAppleBtn = $(selectors.acAppleBtn);
-		$acMobileMenu = $(selectors.acMobileMenu);
-		$acCloseMenu = $(selectors.acCloseMenu);
-		$acIphone = $(selectors.acIphone);
-		$body = $('body');
-		$html = $('html');
-
-		$acLangButton.on('click', function(){
-			var $this = $(this);
-			$this.closest(selectors.acLang).toggleClass(classNames.acOpen);
+	$(function () {
+		$acLang.on('click', function () {
+			$(this).toggleClass('ac-dropdown-show')
 		});
 
-		$acSentAppBtn.on('click', function() {
-			$(this).addClass(classNames.acHide);
-			$acInputBlock.removeClass(classNames.acHide);
+		$acMobileMenu.on('click', function () {
+			$body.addClass('ac-show-menu');
+		});
+		$acCloseMenu.on('click', function () {
+			$body.removeClass('ac-show-menu');
 		});
 
 		if ($html.hasClass('android')) {
@@ -83,27 +97,70 @@
 		}
 
 		if ($html.hasClass('ios')) {
-			$acGoogleBtn.addClass(classNames.acNoUrl);
+			$acAndroidBtn.addClass(classNames.acNoUrl);
 			showWindowSend();
 		}
 
-		$acMobileMenu.on('click', function(){
-			$body.toggleClass(classNames.acShowMenu);
+		$acPopupClose.on('click', function () {
+			$acPopupBox.addClass('hide');
+			noScrollBody();
 		});
 
-		$acCloseMenu.on('click', function(){
-			$body.toggleClass(classNames.acShowMenu);
+		$acSentAppBtn.on('click', function(){
+			$(this).addClass('ac-hide');
+			$acPhoneInputBlock.removeClass('ac-hide');
 		});
+
+		$acPhoneInputBtn.on('click', function(){
+			$acPhoneInputBlock.addClass('ac-hide');
+			$acSendMessageContainer.removeClass('ac-hide');
+			second = defaultTime;
+			$acClock.removeClass('hide');
+			$acSendAgain.addClass('hide');
+			setTimeout(startTimer, 1000);
+		});
+
+		showWindowSend();
+		videoResize();
 		setNameLanguages();
 	});
 
-	$(window).on('resize', setNameLanguages);
+	var startTimer = function() {
+		second--;
+		$acSecond.text('0:' + (second > 9 ? second : '0' + second));
+		if (second === 0) {
+			second = defaultTime;
+			$acSecond.text('0:' + second);
+			$acClock.addClass('hide');
+			$acSendAgain.removeClass('hide');
+			setTimeout(function(){}, 1000);
+		} else{
+			setTimeout(startTimer, 1000);
+		}
+	};
+
+	var showWindowSend = function() {
+		$(selectors.acNoUrl).on('click', function(e){
+			var elem = e.target || e.srcElement;
+			e.preventDefault();
+			$acPopupBox.removeClass('hide');
+			return false;
+		});
+	};
+
+	var videoResize = function() {
+		var documentSize = {
+			height: $(document).height(),
+			width: $(document).height() / 0.5278
+		};
+		$acVideo.css({height: documentSize.height, width: documentSize.width});
+	};
 
 	var setNameLanguages = function() {
 		var $dataSelectLang = $('[data-select-lang="true"]'),
 			$documentWidth = $(document).width(),
 			readParameter;
-		if ($documentWidth <= 960) {
+		if ($documentWidth <= 910) {
 			readParameter = 'full-text';
 		} else {
 			readParameter = 'short-text';
@@ -114,12 +171,8 @@
 		})
 	};
 
-	var showWindowSend = function() {
-		$(selectors.acNoUrl).on('click', function(e){
-			var elem = e.target || e.srcElement;
-			e.preventDefault();
-			alert('No URL');
-			return false;
-		});
-	};
+	$(window).on('resize', function(){
+		videoResize();
+		setNameLanguages();
+	});
 })();
